b384cc4635cbf47f69e01303870520a92c778889
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2025-11-23 01:29:56 +0300

    init: core:model module

---
+++ core/model/src/main/java/ru/kazan/itis/bikmukhametov/model/BookModel.kt
@@ -0,0 +1,17 @@
+package ru.kazan.itis.bikmukhametov.model
+
+import ru.kazan.itis.bikmukhametov.util.enum.BookFormat
+
+data class BookModel(
+    val id: String,
+    val title: String,
+    val author: String,
+    val coverUrl: String? = null,
+    val format: BookFormat,
+    val userId: String,
+    val bucketName: String? = null,
+    val fileUrl: String? = null,
+    val isDownloaded: Boolean = false,
+    val isAvailableInFirestore: Boolean = true,
+    val localFilePath: String? = null
+)
